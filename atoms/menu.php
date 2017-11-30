66464661f521b70f79e82dfc12638b5c3bb0bbe3
Michiel <michiel@sowmedia.nl>
2017-11-30 15:51:27 +0100

    Removed cart options

--- atoms/menu.php
+++ atoms/menu.php
@@ -7,16 +7,13 @@
 $atom = wp_parse_args( $atom, array(
     'all'        => __('View all search results', 'components'),
     'args'       => array(),
-    'cart'       => false,
-    'checkout'   => __('Checkout', 'components'),
     'collapse'   => false,      // If you want to collapse to the mobile menu by default, and expandable with clicking
     'dropdown'   => true,      // If you want to remove dropdowns
     'hamburger'  => 'mobile',   // Accepts mobile (768px), tablet (1024px) always (always hamburger) or false (never hamburger)
     'indicator'  => true,
     'menu'       => '',
     'none'       => __('Bummer! No results found', 'components'),
-    'view'       => '',         // Accepts dark to display a dark mobile menu, fixed, left or right to display the hamburger with a special menu
-    'viewCart'   => __('View Cart', 'components')         
+    'view'       => '',         // Accepts dark to display a dark mobile menu, fixed, left or right to display the hamburger with a special menu      
 ) );
 
 if( $atom['collapse'] )
