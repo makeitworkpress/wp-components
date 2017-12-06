13d3731d3f0d5936d9cc6f3728bd0fc7cc3b34bd
Michiel <michielsite@hotmail.com>
2017-12-06 09:02:43 +0100

    Added support for rounded image and jobtitle in author section

--- atoms/author.php
+++ atoms/author.php
@@ -9,18 +9,21 @@ $atom = wp_parse_args( $atom, array(
     'avatar'            => get_avatar( $post->post_author, 100 ),
     'description'       => get_the_author_meta('description'),
     'imageFloat'        => 'none',
+    'imageRounded'      => true,
+    'jobTitle'          => '',
     'name'              => get_the_author(),
     'prepend'           => '',  // Prepend the author name with a custom description
     'schema'            => 'http://schema.org/Person',
     'url'               => esc_url( get_author_posts_url( $post->post_author ) )
 ) );
 
-?>
+$atom['imageRounded'] = $atom['imageRounded'] ? 'components-rounded' : ''; ?>
+
 <div class="atom-author <?php echo $atom['style']; ?>" itemprop="author" itemscope="itemscope" itemtype="<?php echo $atom['schema']; ?>" <?php echo $atom['inlineStyle']; ?> <?php echo $atom['data']; ?>>
     
     <?php if( $atom['avatar'] ) { ?> 
     
-        <figure class="atom-author-avatar components-<?php echo $atom['imageFloat']; ?>-float components-rounded">
+        <figure class="atom-author-avatar components-<?php echo $atom['imageFloat']; ?>-float <?php echo $atom['imageRounded']; ?>">
             <a class="url fn vcard" href="<?php echo $atom['url']; ?>" rel="author">
                 <?php echo $atom['avatar']; ?>
             </a>
@@ -29,13 +32,17 @@ $atom = wp_parse_args( $atom, array(
     
     <?php } ?>
     
-    <?php if( $atom['description'] || $atom['name'] ) { ?> 
+    <?php if( $atom['description'] || $atom['name'] || $atom['jobTitle'] ) { ?>
     
         <div class="atom-author-description components-<?php echo $atom['imageFloat']; ?>-float">
             
             <?php if( $atom['name'] ) { ?> 
                 <h4 itemprop="name"><?php echo $atom['prepend'] . $atom['name']; ?></h4>
             <?php } ?>
+
+            <?php if( $atom['jobTitle'] ) { ?>
+                <p itemprop="jobTitle"><?php echo $atom['jobTitle']; ?></p>
+            <?php } ?>            
             
             <?php if( $atom['description'] ) { ?>
                 <p itemprop="text"><?php echo $atom['description']; ?></p>
