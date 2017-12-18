38c866bed638c9350922548655bb4e6fbbed7fcc
Michiel <michielsite@hotmail.com>
2017-12-18 14:36:23 +0100

    Better icon for youtube

--- atoms/social.php
+++ atoms/social.php
@@ -14,7 +14,7 @@ $atom = wp_parse_args( $atom, array(
         'twitter'       => 'twitter', 
         'linkedin'      => 'linkedin', 
         'google-plus'   => 'google-plus', 
-        'youtube'       => 'youtube', 
+        'youtube'       => 'youtube=play', 
         'pinterest'     => 'pinterest', 
         'reddit'        => 'reddit-alien', 
         'stumbleupon'   => 'stumbleupon',
