5681ac50ebb964c92f477a69f24b2a3e6b5f66cc
Michiel <michielsite@hotmail.com>
2018-03-01 15:02:17 +0100

    Termlist is dynamic based on taxonomies in a post

--- src/atoms/termlist.php
+++ src/atoms/termlist.php
@@ -5,12 +5,38 @@
 
 // Atom values
 $atom = MakeitWorkPress\WP_Components\Build::multiParseArgs( $atom, [
-    'id'        => get_the_ID(),
-    'taxonomies'      => [
-        'category'      => [ 'after' => '', 'before' => '','icon' => 'folder', 'schema' => 'genre', 'seperator' => ', ' ],
-        'post_tag'      => [ 'after' => '', 'before' => '','icon' => 'tags', 'schema' => 'keywords', 'seperator' => ', ' ]
-    ]
-] ); 
+    'id'                => get_the_ID(),
+    'taxonomies'        => [] // Accepts the following format: 'category' => ['after' => '', 'before' => '','icon' => 'folder', 'schema' => 'genre', 'seperator' => ', ']
+] );
+
+// Show our taxonomies if the standard array is empty
+if( empty($atom['taxonomies']) ) {
+
+    global $post;
+
+    $taxonomies = get_post_taxonomies( $post );
+
+    if( $taxonomies ) {
+        foreach( $taxonomies as $taxonomy ) {
+
+            // Scheme
+            if( $taxonomy == 'category ' ) {
+                $scheme                     = 'genre';
+            } elseif( $taxonomy == 'post_tag' ) {
+                $scheme                     = 'keywords';   
+            } else {
+                $scheme                     = '';
+            }
+
+            // Icons
+            $icon = $taxonomy == 'post_tag' ? 'tag' : 'dot-circle-o';
+
+            $atom['taxonomies'][$taxonomy]  = ['after' => '', 'before' => '','icon' => $icon, 'schema' => $scheme, 'seperator' => ', '];
+
+        }
+    }
+
+}
 
 // Retrieve our lists
 foreach( $atom['taxonomies'] as $taxonomy => $properties ) { 
@@ -22,7 +48,7 @@ $attributes = MakeitWorkPress\WP_Components\Build::attributes($atom['attributes'
 <div <?php echo $attributes; ?>>
     <?php foreach( $atom['taxonomies'] as $taxonomy => $properties ) { ?>
         <?php if( $properties['list'] ) { ?> 
-            <div class="atom-termlist-item entry-<?php echo $taxonomy; ?>" itemprop="<?php echo $properties['schema']; ?>">
+            <div class="atom-termlist-item entry-<?php echo $taxonomy; ?>" <?php if($properties['schema']) { echo 'itemprop="' . $properties['schema'] . '"'; } ?>">
                 <?php if( $properties['icon'] ) { ?>
                     <i class="fa fa-<?php echo $properties['icon']; ?>"></i>
                 <?php } ?>
