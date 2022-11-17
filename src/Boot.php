8812419811a59faf87f62350a67dae53fc75922e
leichim <michielsite@hotmail.com>
2022-11-17 11:40:36 +0100

    Fixed Font Awesome 5 display for some items in css

---
+++ src/Boot.php
@@ -0,0 +1,133 @@
+<?php
+/**
+ * Initialize our components engine
+ */
+namespace MakeitWorkPress\WP_Components;
+
+defined( 'ABSPATH' ) or die( 'Go eat veggies!' );
+
+class Boot {
+
+    /**
+     * Holds our ajax object
+     * 
+     * @access public
+     */
+    public $ajax = null;
+    
+    /**
+     * Holds our configurations
+     * @access private
+     */
+    private $configurations = [];
+    
+    /**
+     * Initialize our components
+     *
+     * @param array $configurations The basic configurations for the components
+     */
+    public function __construct( $configurations = [] ) {
+        
+        // Setup our configurations. By default, we load css and js from the components library.
+        $this->configurations = wp_parse_args( $configurations, [
+            'animate'       => false,           // Whether to load animate.css or not
+            'css'           => true,            // Load standard css
+            'fontawesome'   => true,            // Loads the fontawesome css
+            'hover'         => false,           // Whether to load hover.css or not
+            'js'            => true,            // Load standard js
+            'language'      => 'wp-components', // The default language domain
+            'maps'          => '',              // The API key for Google Maps. 
+            'scrollreveal'  => true,            // Loads the scrollreveal JS
+            'tinyslider'    => true             // Loads the slider js and css
+        ] );
+        
+        // Define Constants
+        $folder = wp_normalize_path( substr( dirname(__DIR__, 1), strpos(__FILE__, 'wp-content') + strlen('wp-content') ) );    
+        defined( 'WP_COMPONENTS_ASSETS' ) or define( 'WP_COMPONENTS_ASSETS', content_url() . $folder . '/assets/' );
+        defined( 'WP_COMPONENTS_PATH' ) or define( 'WP_COMPONENTS_PATH', plugin_dir_path( __FILE__ ) );
+        defined( 'WP_COMPONENTS_LANGUAGE' ) or define( 'WP_COMPONENTS_LANGUAGE', $this->configurations['language'] );
+        
+        // Register our ajax actions
+        $this->ajax = new Ajax();
+
+        // Load our functions
+        require_once( WP_COMPONENTS_PATH . 'includes/functions.php' );
+        
+        // Register Hooks
+        $this->hook();
+        
+    } 
+    
+     
+    /**
+     * Contains our standaard hooks
+     */
+    private function hook(): void {
+        
+        add_action( 'wp_enqueue_scripts', function(): void {
+            
+            // Enqueue tinyslider CSS and JS
+            if( $this->configurations['tinyslider'] ) {
+                wp_enqueue_style( 'tinyslider-css', WP_COMPONENTS_ASSETS . 'vendor/css/tinyslider.min.css');
+                wp_enqueue_script( 'tinyslider-js', WP_COMPONENTS_ASSETS . 'vendor/js/tinyslider.min.js', [], NULL, true);
+            }
+            
+            // Enqueue scrollreveal JS
+            if( $this->configurations['scrollreveal'] ) {
+                wp_enqueue_script( 'scrollreveal-js', WP_COMPONENTS_ASSETS . 'vendor/js/scrollreveal.min.js', [], NULL, true);
+            }           
+            
+            // Enqueue our default components CSS
+            if( $this->configurations['css'] ) {
+                wp_enqueue_style( 'wpc-css', WP_COMPONENTS_ASSETS . 'css/wpc-styles.min.css');
+            }
+
+            if( $this->configurations['fontawesome'] ) {
+                wp_enqueue_style( 'font-awesome', WP_COMPONENTS_ASSETS . 'vendor/css/font-awesome.min.css');
+            }
+            
+            // Enqueue our animate CSS
+            if( $this->configurations['animate'] ) {
+                wp_enqueue_style( 'animate-css', WP_COMPONENTS_ASSETS . 'vendor/css/animate.min.css');
+            } 
+            
+            // Enqueue our hover CSS
+            if( $this->configurations['hover'] ) {
+                wp_enqueue_style( 'hover-css', WP_COMPONENTS_ASSETS . 'vendor/css/hover.min.css');
+            }  
+            
+            // Registers the maps script
+            if( $this->configurations['maps'] ) {
+                wp_register_script( 'google-maps-js', 'https://maps.googleapis.com/maps/api/js?key=' . $this->configurations['maps'], [], '3', true);
+            }               
+            
+            // Enqueue our default components JS
+            if( $this->configurations['js'] ) {
+                
+                wp_enqueue_script( 'wpc-js', WP_COMPONENTS_ASSETS . 'js/wpc-scripts.js', ['jquery'], NULL, true );
+
+                // Localize our script
+                wp_localize_script( 'wpc-js', 'wpc', [
+                    'ajaxUrl' => admin_url( 'admin-ajax.php' ),
+                    'restUrl' => esc_url_raw( rest_url() ),
+                    'debug'   => defined('WP_DEBUG') && WP_DEBUG ? true : false,
+                    'nonce'   => wp_create_nonce( 'cucumber' ),
+                ]);
+
+            }
+            
+        } );     
+        
+        // Specific WooCommerce Based Actions
+        if( class_exists('WooCommerce') ) { 
+            // Counter that updates the mini cart with ajax
+            add_filter('woocommerce_add_to_cart_fragments', function($fragments) {
+                $fragments['span.atom-cart-count'] = '<span class="atom-cart-count">' . WC()->cart->get_cart_contents_count() . '</span>'; 
+                
+                return $fragments;
+            });  
+        }        
+        
+    }   
+
+}
---
+++ src/Build.php
@@ -0,0 +1,405 @@
+<?php
+/**
+ * Load our components using a static wrapper
+ * Adds some functionalities for modifying component properties and parsing arguments
+ */
+namespace MakeitWorkPress\WP_Components;
+use WP_Error as WP_Error;
+
+defined( 'ABSPATH' ) or die( 'Go eat veggies!' );
+
+class Build {
+    
+    /**
+     * Renders generic template for an atom or molecule.
+     *
+     * @param string    $type       The type, either a molecule or atom
+     * @param string    $template   The component to load, either a template in the molecule or atom's folder
+     * @param array     $properties The custom properties for the template    
+     * @param array     $render     If the element is rendered. If set to false, the contents of the elements are returned  
+     * 
+     * @return string|void          The rendered string for the given atom or molecule, but only if render is true
+     */
+    private static function render( string $type, string $template, array $properties = [], bool $render = true ) {
+
+        if( ! in_array($type, ['atom', 'molecule']) ) {
+            $error = new WP_Error( 'wrong', __('The type for rendering should be a molecule or atom.', 'wpc') );
+            echo $error->get_error_message();    
+            return;
+        }
+        
+        // Empty properties can be neglected
+        if( empty($properties) ) {
+            $properties = [];
+        }
+
+        // Properties should be an array
+        if( ! is_array($properties) ) {
+            $error = new WP_Error( 'wrong', sprintf(__('The properties for the molecule or atom called %s are not properly formatted as an array.', 'wpc'), $template) );
+            echo $error->get_error_message();    
+            return;
+        }
+
+        // If we have atom properties, they should have proper properties
+        if( isset($properties['atoms']) && is_array($properties['atoms']) ) {
+            foreach( $properties['atoms'] as $atom ) {
+                if( ! isset($atom['atom']) ) {
+                    $error = new WP_Error( 'wrong', sprintf(__('The custom atoms within %s are not properly formatted and miss the atom key.', 'wpc'), $template) );
+                    echo $error->get_error_message();  
+                    return;
+                }    
+            }
+        }
+
+        // The molecules post-footer and post-header are deprecated. The following code ensures backwards compatibility.
+        if( $template === 'post-footer' || $template === 'post-header' ) {
+            switch($template) {
+                case 'post-footer':
+                    $custom_action  = 'post_footer';
+                    $tag            = 'footer';
+                    break;
+                case 'post-header':
+                    $custom_action  = 'post_header';
+                    $tag            = 'header';
+                    break;
+            }
+
+            $properties['custom_action']    = $custom_action;
+            $properties['tag']              = $tag;
+            $template = 'section';
+            
+        }
+            
+        // Our template path
+        $path = apply_filters( 'components_' . $type . '_path', WP_COMPONENTS_PATH . 'components/' . $type . 's/' . $template . '.php', $template );
+        
+        if( file_exists($path) ) {
+            
+            ${$type}    = apply_filters( 'components_' . $type . '_properties', self::set_default_properties($template, $properties, $type), $template );
+            
+            // If we do not render, we return
+            if( $render == false ) {
+                ob_start();
+            }
+            
+            require($path); 
+            
+            if( $render == false ) {
+                return ob_get_clean();
+            }
+            
+        } else {
+            $error = new WP_Error( 'wrong', sprintf( __('The given template for the molecule or atom called %s does not exist.', 'wpc'), $template ) );
+            echo $error->get_error_message();             
+        }     
+        
+    }
+    
+    /**
+     * Define the default attributes per template. This allows us to dynamically add attributes 
+     * 
+     * @param   string  $template   The molecule or atom template to load
+     * @param   array   $properties The custom properties defined by the developer
+     * @param   string  $type       Whether we load an atom or an molecule
+     *
+     * @return  array   $properties The custom properties merged with the defaults
+     */ 
+    public static function set_default_properties( string $template, array $properties, string $type = 'atom' ): array {
+
+        // Define our most basic property - the class
+        $properties['attributes']['class']  = isset($properties['attributes']['class']) ? $type . ' ' . $properties['attributes']['class'] : $type;
+        $properties['attributes']['class'] .= ' ' . $type . '-' . $template;
+
+        // These are the common properties that each element can have
+        $classes = [
+            'align', 
+            'animation', 
+            'appear', 
+            'background', 
+            'border',
+            'boxshadow', 
+            'color', 
+            'display', 
+            'float', 
+            'grid', 
+            'height', 
+            'hover', 
+            'overlay',
+            'parallax', 
+            'position', 
+            'rounded', 
+            'video', 
+            'width'
+        ];
+
+        /**
+         * Properties that generate a specific class for a style or are generic
+         */
+        foreach( $classes as $class ) {
+            
+            if( isset($properties[$class]) && $properties[$class] ) {
+
+                // Advanced animations using animate.css (should be enabled in the configurations during instance boot as well)
+                if( $class == 'animation' && ! in_array($properties[$class], ['fadein', 'fadeindown', 'slideinleft', 'slideinright']) ) {
+                    $properties['attributes']['class'] .= ' animate__animated animate__' . $properties[$class]; 
+                    continue;
+                }                
+
+                // Backgrounds
+                if( $class == 'background' && preg_match('/hsl|http|https|rgb|linear-gradient|#/', $properties[$class]) ) {
+
+                    if( preg_match('/http|https/', $properties[$class]) ) {
+
+                        $properties['attributes']['class']                         .= ' components-image-background';                   
+                        $properties['attributes']['style']['background-image']      = 'url(' . $properties[$class] . ')';
+
+                    } else {
+                        $properties['attributes']['style']['background']            = $properties[$class];
+                    }
+
+                    continue;
+                }
+
+                // Borders
+                if( $class == 'border' && preg_match('/hsl|linear-gradient|rgb|#/', $properties[$class]) ) {
+                    if( strpos($properties['border'], 'linear-gradient') === 0 ) {
+                        $properties['attributes']['style']['border']                = '2px solid transparent;';
+                        $properties['attributes']['style']['border-image']          = $properties[$class];                        
+                        $properties['attributes']['style']['border-image-slice']    = 1;                        
+                    } else {
+                        $properties['attributes']['style']['border']                = '2px solid ' . $properties[$class];
+                    }
+                    continue;
+                }
+
+                // Box Shadow
+                if( $class == 'boxshadow' && isset($properties[$class]) ) {
+                    
+                    // Custom shadows using CSS attr()
+                    if( is_array($properties[$class]) ) {
+                        $properties['attributes']['class'] .= ' components-custom-boxshadow';
+                        foreach( ['x', 'y', 'blur', 'spread', 'color', 'type'] as $value) {
+                            if( isset($properties[$class][$value]) ) {
+                                $properties['attributes']['data'][$value] = $properties[$class][$value];
+                            }
+                        }
+                    // Predefined shadows    
+                    } elseif( is_string($properties[$class]) ) {
+                        $properties['attributes']['class'] .= ' components-' . $properties[$class] . '-boxshadow';
+                    }
+                    continue;
+                }                
+                
+                // Color
+                if( $class == 'color' && preg_match('/hsl|rgb|#/', $properties[$class]) ) {
+                    $properties['attributes']['style']['color']                     = $properties[$class];
+                    continue;
+                }  
+                
+                // Continue if our grid is an array
+                if( $class == 'grid' && is_array($properties[$class]) ) {
+                    continue;
+                }
+
+                // Height and Width
+                if( ($class == 'height' || $class == 'width') && preg_match('/ch|em|ex|in|mm|pc|pt|px|rem|vh|vw|%/', $properties[$class]) ) {
+                    $properties['attributes']['style']['min-' . $class]             = $properties[$class];
+                    continue;
+                }
+
+                // Advanced hover settings using hover.css (should be enabled in the configurations during instance boot as well)
+                if( $class == 'hover' ) {
+                    $properties['attributes']['class'] .= ' hvr-' . $properties[$class]; 
+                    continue;
+                } 
+                
+                // Overlay
+                if( $class == 'overlay' && isset($properties[$class]) ) {
+                    
+                    // Custom overlays using CSS attr()
+                    if( is_array($properties[$class]) ) {
+                        $properties['attributes']['class'] .= ' components-overlay components-custom-overlay';
+                        foreach( ['color', 'opacity'] as $value) {
+                            if( isset($properties[$class][$value]) ) {
+                                $properties['attributes']['data'][$value] = $properties[$class][$value];
+                            }
+                        }
+                    // Predefined overlays  
+                    } elseif( is_string($properties[$class]) ) {
+                        $properties['attributes']['class'] .= ' components-overlay components-' . $properties[$class] . '-overlay';
+                    }
+                    continue;
+                }                 
+
+                if( $class == 'video' ) {
+                    $properties['attributes']['class'] .= ' components-video-background'; 
+                    continue;
+                }                
+
+                // Set our definite class for other properties
+                $properties['attributes']['class'] .= is_bool($properties[$class]) ? ' components-' . $class : ' components-' . $properties[$class] . '-' . $class;
+
+            }
+        }
+        
+        return $properties;
+        
+    }
+
+    /**
+     * Displays any atom
+     *
+     * @param string    $atom           The atom to load
+     * @param array     $properties     The custom properties for a molecule
+     * 
+     * @return string:|void             The rendered atom
+     */
+    public static function atom( string $atom, array $properties = [], bool $render = true ) {
+        
+        if( $render == false ) {
+            return self::render( 'atom', $atom, $properties, $render );    
+        }
+        
+        self::render( 'atom', $atom, $properties );
+        
+    }
+    
+    /**
+     * Displays any molecule
+     *
+     * @param string    $molecule       The atom to load
+     * @param array     $properties     The custom properties for a molecule
+     * 
+     * @return string:|void             The rendered molecule
+     */
+    public static function molecule( string $molecule, array $properties = [], bool $render = true ) {
+        
+        if( $render == false ) {
+            return self::render( 'molecule', $molecule, $properties, $render );    
+        }
+        
+        self::render( 'molecule', $molecule, $properties );
+        
+    }
+
+    /**
+     * Turns our attributes into a usuable string for use in our atoms
+     * 
+     * @param   array   $attributes The array with custom properties
+     * 
+     * @return  string  $output     The attributes as a string
+     */
+    public static function attributes( array $attributes = [] ): string {
+
+        $output     = '';
+
+        foreach( $attributes as $key => $attribute ) {
+
+            // Skip empty attributes
+            if( ! $attribute ) {
+                continue;
+            }
+
+            if( $key == 'data' && is_array($attribute) ) {
+                foreach( $attribute as $data => $value ) {
+                    $output .= ' data-' . $data . '="' . $value . '"';
+                }
+            } elseif( $key == 'style' && is_array($attribute) ) {
+                $style  = '';
+                foreach( $attribute as $selector => $value ) {
+                    if( ! $value ) {
+                        continue;
+                    }                    
+                    $style .= $selector . ':' . $value . ';';
+                } 
+
+                // Only if we style properties we add our inline styling
+                if( $style ) {
+                    $output .= ' style="' . $style . '"';
+                }
+
+            } else {
+                $output .= ' ' . $key .'="' . $attribute . '"';
+            }
+        }
+
+        return $output;
+
+    }
+
+    /**
+     * This function exists for backwards compatibility for multiParseArgs, may they be used externally
+     * 
+     * @param array $args       The arguments to parse
+     * @param array $default    The default arguments
+     * 
+     * @return array $array     The merged array
+     */
+    public static function multiParseArgs( array $args, array $default ): array {
+        return self::multi_parse_args( $args, $default );
+    }
+
+    /**
+     * Allows us to parse arguments in a multidimensional array
+     * 
+     * @param array $args       The arguments to parse
+     * @param array $default    The default arguments
+     * 
+     * @return array $array     The merged array
+     */
+    public static function multi_parse_args( array $args, array $default ): array {
+
+        if( ! is_array($default) ) {
+            return wp_parse_args( $args, $default );
+        }
+
+        $array = [];
+
+        // Loop through our multidimensional array
+        foreach( [$default, $args] as $elements ) {
+
+            foreach( $elements as $key => $element ) {
+
+                // If we have numbered keys
+                if( is_integer($key) ) {
+                    $array[] = $element;
+
+                // Atoms are always overwritten by the arguments
+                } elseif( in_array($key, ['atoms', 'content_atoms', 'footer_atoms', 'header_atoms', 'image', 'socket_atoms', 'top_atoms']) ) { 
+                    $array[$key] = $element;
+                } elseif( isset( $array[$key] ) && (is_array( $array[$key] )) && ! empty($array[$key]) && is_array($element) ) {
+                    $array[$key] = self::multi_parse_args( $element, $array[$key] );
+                } else {
+                    $array[$key] = $element;
+                }
+
+            }
+        }
+
+        return $array;
+
+    } 
+    
+    /**
+     * Retrieves older variable set-up, using camelcase to the new variations
+     * This function exists for backwards compatibility
+     * 
+     * @param Array $properties     The properties for a component, either a molecule or component
+     * @param Array $converts       The properties that need to be converted, in the format of old => new
+     * 
+     * @return Array $properties    The modified component properties
+     */
+    public static function convert_camels( $properties, $converts ) {
+
+        foreach($converts as $old => $new ) {
+            if( isset($properties[$old]) && $properties[$old] ) {
+                $properties[$new] = $properties[$old];
+                unset($properties[$old]);
+            }
+        }
+
+        return $properties;  
+
+    }
+    
+}
